5bf67d403ee94823f98b9b93f302d6cdf7440519
kk <a631953720@gmail.com>
2022-05-19 22:26:23 +0800

    feat: add mouse event handler

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,9 +6,22 @@ type ButtonProps = {
   disabled: boolean;
   value: string;
   onClick?: MouseEventHandler<HTMLDivElement>;
+  onMouseUp?: MouseEventHandler<HTMLInputElement>;
+  onMouseDown?: MouseEventHandler<HTMLInputElement>;
 };
 
 export const Button = (props: ButtonProps) => {
-  const { name, disabled, value, onClick } = props;
-  return <input type="button" value={value} className="style-button" name={name} disabled={disabled} onClick={onClick} />;
+  const { name, disabled, value, onClick, onMouseDown, onMouseUp } = props;
+  return (
+    <input
+      type="button"
+      value={value}
+      className="style-button"
+      name={name}
+      disabled={disabled}
+      onClick={onClick}
+      onMouseDown={onMouseDown}
+      onMouseUp={onMouseUp}
+    />
+  );
 };
